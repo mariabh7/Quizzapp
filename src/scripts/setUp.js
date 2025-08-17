f805e5d805b6bcbf3979be8984232b1375f21d7a
Maria <bouhrem.maria@gmail.com>
2025-08-17 19:19:23 +0100

    feat: add user data checkout + topic fetching

--- src/scripts/setUp.js
+++ src/scripts/setUp.js
@@ -82,6 +82,31 @@ const userMajor = document.getElementById("Usermajor");
 const topicSel = document.getElementById("topicSel");
 const diffLevel = document.getElementById("diffLevel");
 const NumberOfQA = document.getElementById("NumberOfQA");
+async function GetDataViaAPI() {
+    try {
+        let res = await fetch("https://quizapi.io/api/v1/tags?apiKey=y65cYlPTKSDaUdayGMs2iiRrJjfEUVHKdDTfHTso");
+        let Data = await res.json();
+        try {
+            Data.forEach(element => {
+                let opt = document.createElement("option");
+                if (element.name != "Undefined") {
+                    opt.textContent = element.name;
+                    topicSel.appendChild(opt);
+                }
+            });
+        } catch {
+            let warning = document.createElement("p");
+            warning.textContent = "failed to retrieve topic data ! please try again later "
+            warning.className = " my-10 text-yellow-500"
+            document.getElementById("TechInfo").appendChild(warning);
+        }
+
+    } catch {
+        console.log("problem loading topics");
+    }
+}
+GetDataViaAPI()
+
 NextButton.addEventListener("click", (e) => {
     e.preventDefault();
     if (NextButton.textContent === "submit") {
@@ -99,18 +124,31 @@ PrevButton.addEventListener("click", (e) => {
     showPrevStep();
     SelectContent(singleitem);
 })
+
 SelectContent(singleitem);
 
 function ManageData() {
-    let User = new Userdata(userName.value, userMajor.value, topicSel.value, diffLevel.value, parseInt(NumberOfQA.value));
-    if (users.has(User.name)) {
-        User.setData([diffLevel.value, topicSel.value, parseInt(NumberOfQA.value)])
+    if (userName.value.length == 0 || userMajor.value.length == 0 || topicSel.value == "topic" || diffLevel.value == "level" || parseInt(NumberOfQA.value) == 0) {
+        let warning = document.createElement("p");
+        warning.textContent = "your submittion failed , please provide Infos"
+        warning.className = " my-10 text-red-500"
+        document.getElementById("forms").appendChild(warning)
+        setTimeout(() => {
+            warning.remove();
+        }, 5000);
     } else {
-        users.set(User.name, User);
+        let User = new Userdata(userName.value, userMajor.value, topicSel.value, diffLevel.value, parseInt(NumberOfQA.value));
+        if (users.has(User.name)) {
+            User.setData([diffLevel.value, topicSel.value, parseInt(NumberOfQA.value)])
+        } else {
+            users.set(User.name, User);
+        }
+        console.log(User);
+        localStorage.setItem("usersName", User.name);
+        document.body.classList.add("fade-out");
+        setTimeout(() => {
+            window.location.href = "home.html";
+        }, 400);
     }
-    localStorage.setItem("usersName", User.name);
-    document.body.classList.add("fade-out");
-    setTimeout(() => {
-        window.location.href = "home.html";
-    }, 400);
+
 }
